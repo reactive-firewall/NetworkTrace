5ed399b6af44529148f77354a37af5fc194acfe0
reactive-firewall <reactive-firewall@users.noreply.github.com>
2016-08-04 00:26:30 -0700

    Version 0.1.2 Network Trace Scripts
    Signed-off-by: reactive-firewall <reactive-firewall@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='NetworkTraceScripts',
-    version='0.1.0',
+    version='0.1.2',
     description='Network Trace Scripts from reactive-firewall',
     long_description=readme,
     author='reactive-firewall',
